6c1db28bd9bfe66a381aff3acdc3aa2795f6f86f
Sergiy Popovych <popovych@princeton.edu>
2018-03-09 18:07:25 -0500

    -y

--- get_ndock.sh
+++ get_ndock.sh
@@ -12,12 +12,12 @@ sudo add-apt-repository \
           $(lsb_release -cs) \
              stable"
 sudo apt-get update
-sudo apt-get install docker-ce=17.12.1~ce-0~ubuntu
+sudo apt-get install docker-ce=17.12.1~ce-0~ubuntu -y
 
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
       sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/ubuntu16.04/amd64/nvidia-docker.list | \
       sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 sudo apt-get update
-sudo apt-get install nvidia-docker2
+sudo apt-get install nvidia-docker2 -y
 sudo pkill -SIGHUP dockerd
